88cc4dbdbb15b131eab679cf9f72771fe2445e26
UnaJeon <unajeon@gmail.com>
2020-05-14 05:57:47 +0000

    added events for NY and css change

--- components/app.js
+++ components/app.js
@@ -22,13 +22,25 @@ class App{
     error: this.handleGetEventsError
   })
 }
+getNyEvents(){
+  $.ajax({
+    method: "GET",
+    url: "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=345&apikey=ga0wMLksMl8VwL5tzAT9bH0rW4YstsPx",
+    async: true,
+    dataType: "json",
+    success: this.handleGetEventsSuccess,
+    error: this.handleGetEventsError
+  })
+}
 sendMessage(){
-  var message = document.getElementById("textarea").value;
-  var username = "fc247190fc7c5dec0cb367b41d9f7af6";
-  var password = "958c01f502889fb0bbc1933255d6dd14";
+  var messageBox =document.getElementById("textarea")
+  var message = messageBox.value;
+  document.querySelector('button.close').click();
+  var username = "ACea444de4ea87bf7afc0c73eb7935d06f";
+  var password = "fc247190fc7c5dec0cb367b41d9f7af6";
   $.ajax({
     method: "POST",
-    url: "https://api.twilio.com/2010-04-01/Accounts/fc247190fc7c5dec0cb367b41d9f7af6/Messages.json",
+    url: "https://api.twilio.com/2010-04-01/Accounts/ACea444de4ea87bf7afc0c73eb7935d06f/Messages.json",
     data: {
       "To": '+17142805279',
       "From": "+12513060563",
@@ -42,19 +54,12 @@ sendMessage(){
     },
     error: this.handleGetEventsError
   })
-
 }
 start(){
   this.eventsTable.onSendMessage(this.sendMessage)
   this.getEvents();
+  this.getNyEvents();
 
   //this.sendMessage('7142805279');
 }
 }
-// Pop a modal when user clicks send a message
-// when user clicks send message
-// unhide modal
-// modal should have text area for message
-// modal should have submit button to send message
-// when modal submit button is clicked
-// sendMessage function is called, and passed users message
--- main.js
+++ main.js
@@ -1,4 +1,28 @@
+
+
 var table = document.querySelector(".table");
 var eventsTable = new EventsTable(table);
 var app = new App(eventsTable);
 app.start();
+
+$(document).ready(initializeApp);
+
+function initializeApp(){
+  $(".container").hide();
+  $(".secondContainer").hide();
+}
+$(document).ready(function(){
+  $("#la_button").click(function(){
+    $(".container").show();
+    $(".landingPage").hide();
+    $(".secondContainer").hide();
+  });
+  $("#ny_button").click(function(){
+    $(".secondContainer").show();
+    $(".landingPage").hide();
+    $(".container").hide();
+  })
+});
+
+
+//
